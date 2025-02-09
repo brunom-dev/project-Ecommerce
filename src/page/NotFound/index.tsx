2a0d9f3bf73394eb3505ab91fa6b3a171f43a1f5
Bruno Macedo <devbrunomacedo@gmail.com>
2025-02-09 13:02:26 -0300

    feat: finalizacao da interface da pagina de Not Found

--- src/page/NotFound/index.tsx
+++ src/page/NotFound/index.tsx
@@ -1,7 +1,10 @@
+import notFoundImage from "../../assets//images/notfound.svg"
+
 export function NotFound() {
     return(
-        <>
-            <h1>Error Pagina nao encontrada</h1>
-        </>
+        <div className='flex justify-center flex-col items-center w-full mt-16'>
+            <img src={notFoundImage} alt="ilustracao de personagem com caixa vazia" width={300} />
+            <p className='font-semibold text-2xl'>Ops, pagina não encontrada!</p>
+        </div>    
     )
 }
