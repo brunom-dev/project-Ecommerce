8b08380de745decb5576f7ceb33ba10657385994
Bruno Macedo <devbrunomacedo@gmail.com>
2025-03-24 18:00:21 -0300

    ajustes no footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-sky-600 text-white py-4 mt-5">
+        <footer className="bg-sky-600 text-white py-4 mt-5 inline-flex w-full bottom-16">
             <div className="container mx-auto text-center">
                 <p className="text-lg font-bold">Tech Store</p>
                 <p className="text-sm mt-1 font-semibold">© 2025 Todos os direitos reservados.</p>
