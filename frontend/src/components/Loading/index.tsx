bb1c77bac5e3a41afd32bc5ba75262ca1ab39bfe
Bruno Macedo <devbrunomacedo@gmail.com>
2025-03-24 18:00:32 -0300

    feat: componente de loading

---
+++ frontend/src/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+import "./loading.css";
+
+export function Loading() {
+    return (
+    <div className="box-loading">
+                <div className="loader-circle"></div>
+     </div>
+    );
+}
